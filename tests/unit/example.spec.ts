bf923b447ec531431e71d58e11b91b4a1c742061
lvzhenbo <294221990@qq.com>
2023-11-17 14:10:52 +0800

    更新依赖

--- tests/unit/example.spec.ts
+++ tests/unit/example.spec.ts
@@ -1,10 +1,10 @@
-import { mount } from '@vue/test-utils'
-import HomePage from '@/views/HomePage.vue'
-import { describe, expect, test } from 'vitest'
+import { mount } from '@vue/test-utils';
+import HomePage from '@/views/Home/index.vue';
+import { describe, expect, test } from 'vitest';
 
 describe('HomePage.vue', () => {
   test('renders home vue', () => {
-    const wrapper = mount(HomePage)
-    expect(wrapper.text()).toMatch('Ready to create an app?')
-  })
-})
+    const wrapper = mount(HomePage);
+    expect(wrapper.text()).toMatch('Ready to create an app?');
+  });
+});

3c8352535c69eddcd8824b9a3aeb4fbbe8c36534
lvzhenbo <294221990@qq.com>
2023-12-01 11:36:09 +0800

    将编辑器命令栏移到底部，同时添加键盘监听器解决被键盘覆盖问题
    由于移动端网页无法登录，所以暂时不解决移动端网页被键盘覆盖问题

--- src/components/Editor/Editor.vue
+++ src/components/Editor/Editor.vue
@@ -1,51 +1,5 @@
 <template>
   <div class="p-1 border w-full min-h-[8rem] overflow-scroll rounded">
-    <div class="flex overflow-x-auto">
-      <div class="icon ion-activatable" @click="editorCMD?.setColor('#958DF1').run()">
-        <IonIcon :icon="colorPalette" />
-        <IonRippleEffect />
-      </div>
-      <div class="icon ion-activatable" @click="editorCMD?.toggleBold().run()">
-        <IonIcon :src="BoldOutlined" :color="editor?.isActive('bold') ? 'primary' : ''" />
-        <IonRippleEffect />
-      </div>
-      <div class="icon ion-activatable" @click="editorCMD?.toggleItalic().run()">
-        <IonIcon :src="ItalicOutlined" :color="editor?.isActive('italic') ? 'primary' : ''" />
-        <IonRippleEffect />
-      </div>
-      <div class="icon ion-activatable" @click="editorCMD?.toggleUnderline().run()">
-        <IonIcon :src="UnderlineOutlined" :color="editor?.isActive('underline') ? 'primary' : ''" />
-        <IonRippleEffect />
-      </div>
-      <div class="icon ion-activatable" @click="editorCMD?.toggleStrike().run()">
-        <IonIcon
-          :src="StrikethroughOutlined"
-          :color="editor?.isActive('strike') ? 'primary' : ''"
-        />
-        <IonRippleEffect />
-      </div>
-      <div class="icon ion-activatable" @click="editorCMD?.setTextAlign('left').run()">
-        <IonIcon
-          :src="AlignLeftOutlined"
-          :color="editor?.isActive({ textAlign: 'left' }) ? 'primary' : ''"
-        />
-        <IonRippleEffect />
-      </div>
-      <div class="icon ion-activatable" @click="editorCMD?.setTextAlign('center').run()">
-        <IonIcon
-          :src="AlignCenterOutlined"
-          :color="editor?.isActive({ textAlign: 'center' }) ? 'primary' : ''"
-        />
-        <IonRippleEffect />
-      </div>
-      <div class="icon ion-activatable" @click="editorCMD?.setTextAlign('right').run()">
-        <IonIcon
-          :src="AlignRightOutlined"
-          :color="editor?.isActive({ textAlign: 'right' }) ? 'primary' : ''"
-        />
-        <IonRippleEffect />
-      </div>
-    </div>
     <EditorContent :editor="editor" />
   </div>
 </template>
@@ -57,14 +11,6 @@
   import TextStyle from '@tiptap/extension-text-style';
   import Underline from '@tiptap/extension-underline';
   import TextAlign from '@tiptap/extension-text-align';
-  import { colorPalette } from 'ionicons/icons';
-  import BoldOutlined from '@/assets/svg/BoldOutlined.svg';
-  import ItalicOutlined from '@/assets/svg/ItalicOutlined.svg';
-  import UnderlineOutlined from '@/assets/svg/UnderlineOutlined.svg';
-  import StrikethroughOutlined from '@/assets/svg/StrikethroughOutlined.svg';
-  import AlignLeftOutlined from '@/assets/svg/AlignLeftOutlined.svg';
-  import AlignCenterOutlined from '@/assets/svg/AlignCenterOutlined.svg';
-  import AlignRightOutlined from '@/assets/svg/AlignRightOutlined.svg';
 
   defineOptions({
     name: 'EditorComponent',
@@ -95,10 +41,11 @@
   });
 
   const editorCMD = computed(() => editor.value?.chain().focus());
+
+  defineExpose({
+    editor,
+    editorCMD,
+  });
 </script>
 
-<style scoped>
-  .icon {
-    @apply relative rounded-full overflow-hidden text-xl w-8 h-8 flex justify-center items-center;
-  }
-</style>
+<style scoped></style>
--- src/views/Thread/add.vue
+++ src/views/Thread/add.vue
@@ -16,22 +16,115 @@
         <IonItem>
           <IonLabel>
             正文
-            <Editor v-model="content" />
+            <Editor ref="editorRef" v-model="content" />
           </IonLabel>
         </IonItem>
       </IonList>
+      <div slot="fixed" class="cmd">
+        <div class="icon ion-activatable" @click="editorRef?.editorCMD?.setColor('#958DF1').run()">
+          <IonIcon :icon="colorPalette" />
+          <IonRippleEffect />
+        </div>
+        <div class="icon ion-activatable" @click="editorRef?.editorCMD?.toggleBold().run()">
+          <IonIcon
+            :src="BoldOutlined"
+            :color="editorRef?.editor?.isActive('bold') ? 'primary' : ''"
+          />
+          <IonRippleEffect />
+        </div>
+        <div class="icon ion-activatable" @click="editorRef?.editorCMD?.toggleItalic().run()">
+          <IonIcon
+            :src="ItalicOutlined"
+            :color="editorRef?.editor?.isActive('italic') ? 'primary' : ''"
+          />
+          <IonRippleEffect />
+        </div>
+        <div class="icon ion-activatable" @click="editorRef?.editorCMD?.toggleUnderline().run()">
+          <IonIcon
+            :src="UnderlineOutlined"
+            :color="editorRef?.editor?.isActive('underline') ? 'primary' : ''"
+          />
+          <IonRippleEffect />
+        </div>
+        <div class="icon ion-activatable" @click="editorRef?.editorCMD?.toggleStrike().run()">
+          <IonIcon
+            :src="StrikethroughOutlined"
+            :color="editorRef?.editor?.isActive('strike') ? 'primary' : ''"
+          />
+          <IonRippleEffect />
+        </div>
+        <div class="icon ion-activatable" @click="editorRef?.editorCMD?.setTextAlign('left').run()">
+          <IonIcon
+            :src="AlignLeftOutlined"
+            :color="editorRef?.editor?.isActive({ textAlign: 'left' }) ? 'primary' : ''"
+          />
+          <IonRippleEffect />
+        </div>
+        <div
+          class="icon ion-activatable"
+          @click="editorRef?.editorCMD?.setTextAlign('center').run()"
+        >
+          <IonIcon
+            :src="AlignCenterOutlined"
+            :color="editorRef?.editor?.isActive({ textAlign: 'center' }) ? 'primary' : ''"
+          />
+          <IonRippleEffect />
+        </div>
+        <div
+          class="icon ion-activatable"
+          @click="editorRef?.editorCMD?.setTextAlign('right').run()"
+        >
+          <IonIcon
+            :src="AlignRightOutlined"
+            :color="editorRef?.editor?.isActive({ textAlign: 'right' }) ? 'primary' : ''"
+          />
+          <IonRippleEffect />
+        </div>
+      </div>
     </IonContent>
   </IonPage>
 </template>
 
 <script setup lang="ts">
   import { htmlToBBCode } from '@/utils/html2bbcode';
+  import BoldOutlined from '@/assets/svg/BoldOutlined.svg';
+  import ItalicOutlined from '@/assets/svg/ItalicOutlined.svg';
+  import UnderlineOutlined from '@/assets/svg/UnderlineOutlined.svg';
+  import StrikethroughOutlined from '@/assets/svg/StrikethroughOutlined.svg';
+  import AlignLeftOutlined from '@/assets/svg/AlignLeftOutlined.svg';
+  import AlignCenterOutlined from '@/assets/svg/AlignCenterOutlined.svg';
+  import AlignRightOutlined from '@/assets/svg/AlignRightOutlined.svg';
+  import Editor from '@/components/Editor/Editor.vue';
+  import { colorPalette } from 'ionicons/icons';
+  import { Keyboard } from '@capacitor/keyboard';
+  import { isPlatform } from '@ionic/vue';
 
   defineOptions({
     name: 'ThreadAdd',
   });
 
   const content = ref('');
+  const editorRef = ref<InstanceType<typeof Editor> | null>();
+  const keyboardHeight = ref('0px');
+
+  onMounted(() => {
+    // 因 Web 端跨域问题导致不可用，暂时不考虑 web 端
+    if (isPlatform('hybrid')) {
+      Keyboard.addListener('keyboardWillShow', (info) => {
+        console.log('keyboard will show with height:', info.keyboardHeight);
+        keyboardHeight.value = info.keyboardHeight + 'px';
+      });
+      Keyboard.addListener('keyboardWillHide', () => {
+        console.log('keyboard will hide');
+        keyboardHeight.value = '0px';
+      });
+    }
+  });
+  onUnmounted(() => {
+    if (isPlatform('hybrid')) {
+      Keyboard.removeAllListeners();
+    }
+  });
 
   watch(content, (value) => {
     console.log(value);
@@ -39,4 +132,11 @@
   });
 </script>
 
-<style scoped></style>
+<style scoped>
+  .icon {
+    @apply relative rounded-full overflow-hidden text-xl w-8 h-8 flex justify-center items-center;
+  }
+  .cmd {
+    @apply flex overflow-x-auto w-full bottom-[v-bind(keyboardHeight)] transition-all duration-150 p-2 bg-[--ion-background-color] shadow-md text-[--ion-text-color];
+  }
+</style>
